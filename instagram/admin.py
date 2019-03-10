5b3bc9cbc7cb5183c4dda646b4c8168f06d174e5
drewheathens <mudavadie@gmail.com>
2019-03-10 15:41:56 +0300

    modified admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Post,Location
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Location)

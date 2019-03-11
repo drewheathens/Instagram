3b3e27ea0b93cdcb2d95a24566690032cd706f7f
drewheathens <mudavadie@gmail.com>
2019-03-11 17:47:26 +0300

    modified settings

--- images/settings.py
+++ images/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l-d_f!+6a*p0qbn+%hhvx4!-0ktd%1ll!ajfi!x7hzg3v37dik'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
